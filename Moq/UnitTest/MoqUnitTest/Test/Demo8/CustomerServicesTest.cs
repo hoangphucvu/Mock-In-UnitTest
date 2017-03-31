a1b487f66b0d2b547df8a2d6c1093aed7bbe5efd
ngohungphuc <ngohungphuc95@gmail.com>
2017-03-31 11:23:07 +0700

    Exception

---
+++ Moq/UnitTest/MoqUnitTest/Test/Demo8/CustomerServicesTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MoqUnitTest.Code.Demo08;
+
+namespace MoqUnitTest.Test.Demo8
+{
+    [TestClass]
+    public class CustomerServicesTest
+    {
+        //verify that specific parameter values are passed to the mock object
+        [TestMethod]
+        [ExpectedException(typeof(CustomerCreationException))]
+        public void An_Exception_Should_Be_Raised()
+        {
+            //Arrange
+
+
+            var mockCustomerRepository = new Mock<ICustomerRepository>();
+            var mockCustomerAddressFactory = new Mock<ICustomerAddressFactory>();
+
+            mockCustomerAddressFactory
+                .Setup(x => x.From(It.IsAny<CustomerToCreateDto>()))
+                .Throws<InvalidCustomerAddressException>();
+
+            var customerService = new CustomerService(mockCustomerRepository.Object, mockCustomerAddressFactory.Object);
+            //Act
+            customerService.Create(new CustomerToCreateDto());
+
+            //Assert
+
+
+        }
+    }
+}
