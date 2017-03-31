6273a315413bcc0e5d4ce3c5c6a699ef27cb4751
ngohungphuc <ngohungphuc95@gmail.com>
2017-03-31 08:32:54 +0700

    out params

---
+++ Moq/UnitTest/MoqUnitTest/Test/Demo4/CustomerServicesTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MoqUnitTest.Code.Demo04;
+
+namespace MoqUnitTest.Test.Demo4
+{
+    [TestClass]
+    public class CustomerServicesTest
+    {
+        [TestMethod]
+        public void The_Customer_Should_Be_Persisted()
+        {
+            //Arrange
+            var mockCustomerRepository = new Mock<ICustomerRepository>();
+            var mockMailingAddressFactory = new Mock<IMailingAddressFactory>();
+
+            var mailingAddress = new MailingAddress { Country = "Canada" };
+
+            mockMailingAddressFactory
+                .Setup(x => x.TryParse(It.IsAny<string>(), out mailingAddress))
+                .Returns(true);
+
+            var customerService = new CustomerService(mockCustomerRepository.Object, mockMailingAddressFactory.Object);
+
+            //Act
+            customerService.Create(new CustomerToCreateDto());
+
+            //Assert
+            mockCustomerRepository.Verify(x => x.Save(It.IsAny<Customer>()));
+
+        }
+
+    }
+}
