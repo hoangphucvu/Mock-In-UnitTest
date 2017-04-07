f4642da0b3c33265de773a1bcae3e077070c1ab9
ngohungphuc <ngohungphuc95@gmail.com>
2017-04-07 12:02:46 +0700

    todo fix assembly error

---
+++ WebDriver/WebDriver/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace WebDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(@"E:\Source Code\Study\.Net-UnitTest\WebDriver\WebDriver\Driver\chromedriver.exe");
+            driver.Url = "http://google.com";
+        }
+    }
+}
