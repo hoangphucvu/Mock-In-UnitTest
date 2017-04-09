a30f074c6d674264a49738ca99708d48b5aa2ffc
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-04-09 23:48:50 +0700

    Mocking Property Getters

---
+++ Moq/UnitTest/MoqUnitTest/Test/Demo10/CustomerServicesTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoqUnitTest.Code.Demo10;
+using Moq;
+
+namespace MoqUnitTest.Test.Demo10
+{
+    [TestClass]
+    public class CustomerServicesTest
+    {
+        //verify that specific parameter values are passed to the mock object
+        [TestMethod]
+        public void The_WorkstationCreatedOn_Shoud_Be_Used()
+        {
+            //Arrange
+            var mockCustomerRepository = new Mock<ICustomerRepository>();
+            var mockApplicationSettings = new Mock<IApplicationSettings>();
+
+            mockApplicationSettings.Setup(x => x.WorkstationId).Returns(123);
+
+            var customerService = new CustomerService(
+                mockCustomerRepository.Object,
+                mockApplicationSettings.Object);
+
+            //Act
+            customerService.Create(new CustomerToCreateDto());
+
+            //Assert
+            mockApplicationSettings.VerifyGet(x => x.WorkstationId);
+        }
+    }
+}
